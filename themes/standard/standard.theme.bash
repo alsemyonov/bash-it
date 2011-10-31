d2a2873736c567d40af03156f24dfd1aa1e5ebc8
Alexander Semyonov <al@semyonov.us>
2011-10-31 04:58:35 +0400

    -m

--- themes/standard/standard.theme.bash
+++ themes/standard/standard.theme.bash
@@ -1,11 +1,4 @@
-# scm themeing
-SCM_THEME_PROMPT_DIRTY="×"
-SCM_THEME_PROMPT_CLEAN="✓"
-SCM_THEME_PROMPT_PREFIX=""
-SCM_THEME_PROMPT_SUFFIX=""
-
-# TODO: need a check for OS before adding this to the prompt
-# ${debian_chroot:+($debian_chroot)}
+#!/bin/bash
 
 #added TITLEBAR for updating the tab and window titles with the pwd
 case $TERM in
@@ -16,9 +9,11 @@ case $TERM in
 	TITLEBAR=""
 	;;
 esac
+PROMPT="${green}\u${normal}@${green}\h${normal}:${blue}\w${normal}${red}\$(scm_char)\$(scm_prompt_info)${reset_color}\$ "
 
-function prompt_command() {
-    PROMPT='${green}\u${normal}@${green}\h${normal}:${blue}\w${normal}${red}$(prompt_char)$(git_prompt_info)${normal}\$ '
-}
-
-PROMPT_COMMAND=prompt_command;
+# scm themeing
+SCM_THEME_PROMPT_DIRTY="×"
+SCM_THEME_PROMPT_CLEAN="✓"
+SCM_THEME_PROMPT_PREFIX=""
+SCM_THEME_PROMPT_SUFFIX=""
+SCM_NONE_CHAR=''
